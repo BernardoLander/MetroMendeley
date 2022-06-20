50d578f408a26fd9f750e31d494451b099c23e77
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2022-06-20 18:46:48 -0400

    Inicio
    Creo

---
+++ src/metromendeley/MetroMendeley.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metromendeley;
+
+/**
+ *
+ * @author leste
+ */
+public class MetroMendeley {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("gola");
+        System.out.println("p");
+    }
+    
+}
