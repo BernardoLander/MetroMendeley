a1c635ec08bc8b60f73e92aa5d3aa3a582fe7ead
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2022-06-21 18:46:55 -0400

    6-21-2022 6:46pm
    Se Crearon los resumenes

--- src/metromendeley/MetroMendeley.java
+++ src/metromendeley/MetroMendeley.java
@@ -1,24 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package metromendeley;
 
-/**
- *
- * @author leste
- */
 public class MetroMendeley {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
-        System.out.println("hola");
-        System.out.println("gola");
-        System.out.println("p");
-        System.out.println("j");
+        Functions functions = new Functions();
+        String w = functions.readText();
+        System.out.println(w);
     }
     
 }
